085ab06b0ad2817fa1ab1a313f28eb9dbf763aab
nnao45 <n4sekai5y@gmail.com>
2019-12-15 21:46:32 +0900

    1個目を書いた

---
+++ src/main/scala/welcome/Main.scala
@@ -0,0 +1,12 @@
+package welcome
+
+import scala.io.StdIn._
+
+object Main extends App {
+  var input1 = readInt
+  var input2 = readLine.split(" ")
+  var input3 = readLine
+
+  var sum = input1 + input2(0).toInt + input2(1).toInt
+  println(s"$sum $input3")
+}
