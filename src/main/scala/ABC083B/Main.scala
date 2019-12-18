733744c94c23862213f46351e9688ae7b56f9908
nnao45 <n4sekai5y@gmail.com>
2019-12-18 12:48:00 +0900

    ABC083B

---
+++ src/main/scala/ABC083B/Main.scala
@@ -0,0 +1,30 @@
+package ABC083B
+
+import scala.io.StdIn.readLine
+
+// https://atcoder.jp/contests/abs/tasks/abc083_b
+
+// 1以上N 以下の整数のうち、10 進法での各桁の和がA 以上B 以下であるものの総和を求めてください。
+
+object Main extends App {
+  def separateNum(input: String): List[Int] =
+    input.toCharArray.map(_.toString.toInt).toList
+
+  def createPrimeList(input: Int, result: List[Int]): List[Int] =
+    if (input > 0) createPrimeList(input - 1, result :+ input) else result
+
+  val inputRaw = readLine
+  val input = inputRaw.split(" ")
+  val inputN = input(0)
+  val inputPrimeList = createPrimeList(input(0).toInt, List())
+  val inputA = input(1).toInt
+  val inputB = input(2).toInt
+  var counter = 0
+
+  inputPrimeList.foreach(p => {
+    val sum = separateNum(p.toString).sum
+    if (sum >= inputA && sum <= inputB) counter += p
+  })
+
+  println(counter)
+}
